f9d30eb0ccc80a520179223fd25f5b93fdba7494
Jacob Gerber <jacogerb@iu.edu>
2018-06-12 13:12:48 -0400

    add styles to Message component

--- src/Message.js
+++ src/Message.js
@@ -2,10 +2,35 @@ import React from 'react'
 
 const Message = (props) => {
     return (
-        <div className="Message">
-           {props.message.userName}: {props.message.body}
+        <div className="Message" style={styles.message}>
+            <div
+                classList="Avatar"
+            ></div>
+            <div classList="details" style={styles.details}>
+                <div classList="Metadata">
+                    <div classList="user">{props.message.userName}</div>
+                    <div classList="time">1:10 PM</div>
+                </div>
+                <div classList="body">
+                    {props.message.body}
+                </div>
+            </div>
+            :
         </div>
     )
+}
+
+const styles = {
+    message: {
+        display: 'flex',
+        marginTop: '1rem',
+        padding: '0 1rem',
+    },
+
+    details: {
+        flex: '1',
+        paddingLeft: '0.5rem',
+    }
 }
 
 export default Message
